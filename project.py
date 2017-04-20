a01b22a49fe4995bfc4acbec6e81fac54b1b1de2
Daniel Walerzak <danexhd@gmail.com>
2017-04-20 16:26:21 +0200

    Add files via upload

--- project.py
+++ project.py
@@ -2,17 +2,27 @@ x = 5
 
 print("Hello World")
 
-print (x)
-
-x = x + 10
+print (type(x))
 
 print (x)
+
+print (type(x))
+
+x = x + 10 * 2.5 / 2
+
+print (x)
+
+print (type(x))
 
 y = 5
 
 print (x+y)
 
+print (type(x))
+
 print ("End, not yet...")
+
+print (type(x))
 
 d = x+y
 
@@ -21,3 +31,43 @@ b = (d+x)
 print (d)
 
 print (b+d)
+
+k = ("Okay")
+
+print (k)
+
+p = (" Thank You")
+
+print (k+ p)
+
+print (type(x))
+
+print (type(k))
+
+print (type(p))
+
+print (type(y))
+
+u = input()
+
+print(type(u))
+
+u = float (u)
+
+u += 10
+
+print (u)
+
+print(type(u))
+
+g = 20
+
+print(type(g))
+
+g += 5
+
+print (g)
+
+print (type(g))
+
+
